a57018a73cf0b07b5d31447fdf400dc92ef09ba6
neritoo <ezegavilan95@gmail.com>
2020-06-22 00:29:43 -0300

    Angular material agregado.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { NgbPaginationModule } from "@ng-bootstrap/ng-bootstrap";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +15,7 @@ import { ArticulosComponent } from './components/articulos/articulos.component';
 import { DetalleArticuloComponent } from './components/articulos/detalle-articulo.component';
 import { ArticulosFamiliasComponent } from './components/articulos-familias/articulos-familias.component';
 import { FormArticuloComponent } from './components/articulos/form-articulo/form-articulo.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -29,7 +33,11 @@ import { FormArticuloComponent } from './components/articulos/form-articulo/form
     HttpClientModule,
     ReactiveFormsModule,
     FormsModule,
-    NgbPaginationModule
+    NgbPaginationModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
